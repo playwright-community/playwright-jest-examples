3dd5ee405c68127ea3aab4ff05ada8d0947b60aa
Max Schmitt <max@schmitt.mx>
2020-06-25 00:29:30 +0200

    ci: run examples on Windows (#10)

--- create-react-app-coverage/jest-playwright.config.js
+++ create-react-app-coverage/jest-playwright.config.js
@@ -2,10 +2,15 @@
 module.exports = {
     browsers: ['chromium', 'firefox', 'webkit'],
     serverOptions: {
-        command: `BROWSER=none npm start`,
+        command: `npm start`,
         port: 3000,
         launchTimeout: 10000,
         debug: true,
+        options: {
+            env: {
+                "BROWSER": "none",
+            }
+        }
     },
     launchOptions: {
         headless: true
