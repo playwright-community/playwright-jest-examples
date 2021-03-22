af8335944c0b95c71d6d3d8cb1af8b40ce495dc7
ocavue <ocavue@gmail.com>
2021-03-22 12:55:22 +0800

    feat: add example for Next.js

---
+++ nextjs-coverage/src/pages/index.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import App from '../components/App'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.title}>
+          <App/>
+        </div>
+
+        <p>debug: NODE_ENV = {process.env.NODE_ENV}</p>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
