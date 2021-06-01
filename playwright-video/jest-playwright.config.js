ea8be568827d58bffe92e6bebc9025f74f89f196
Max Schmitt <max@schmitt.mx>
2021-06-01 03:44:10 -0700

    chore: bump to jest-playwright 1.6.0 and PW 1.11 (#25)

--- playwright-video/jest-playwright.config.js
+++ playwright-video/jest-playwright.config.js
@@ -1,6 +1,7 @@
 // https://github.com/playwright-community/jest-playwright/#configuration
 module.exports = {
   browsers: ["chromium"],
+  launchType: "LAUNCH",
   launchOptions: {
     headless: true
   },
