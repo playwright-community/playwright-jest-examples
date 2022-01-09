86cf1e5b35ef2c60e7af7ea9f4ddeebb3b5b0e59
kelvintaywl <kelvintaywl@gmail.com>
2022-01-09 23:22:27 +0900

    fix: correct filepath

--- www/xml_corrector.py
+++ www/xml_corrector.py
@@ -9,7 +9,7 @@ test_suites = tree.getroot()
 
 for test_suite in test_suites:
     file_name = test_suite.attrib["name"]
-    full_fp = os.path.join(file_dir, file_name)
+    full_fp = os.path.join(file_dir, "tests", file_name)
     for test_case in test_suite:
         test_case.set("file", full_fp)
 
